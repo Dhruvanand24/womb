f7512efaf6e2dbf3acaeca5307c52f8674a02d18
Dhruv_Anand <ananddhruv475@gmail.com>
2023-11-28 19:18:36 +0530

    a

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from '../components/login/login';
 import Admin from '../components/admin/admin';
 import {
@@ -10,13 +10,13 @@ import {
 import Roles from '../components/roles/roles';
 
 const MyRoutes = () => {
-    
+
   return (
     <Router>
     <div className="App">
     <Routes>
           <Route path="/"
-            element={window.localStorage.getItem("isLoggedIn")==="true" ? (<Navigate to="/admin" />) : (<Login />)} />
+            element={(window.localStorage.getItem("isLoggedIn")==="true")?(<Navigate to="/admin" />):<Login /> } />
          
           <Route
             path="/admin"
